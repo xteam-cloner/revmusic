2ef98bc738fd589927c4c8ff7804677399a76459
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2025-09-27 15:38:56 +0530

    Revert "Update ytDetails.py"
    This reverts commit e04b1e33b233974ce443c032eff4f86b31a36e0c.

--- YMusic/utils/ytDetails.py
+++ YMusic/utils/ytDetails.py
@@ -1,4 +1,4 @@
-from curl_cffi.requests import AsyncSession
+import aiohttp
 from pytubefix import Search, YouTube as pyYouTube, Playlist
 from urllib.parse import urlparse, parse_qs
 
@@ -26,28 +26,39 @@ def searchYt(query, is_videoId=False):
 
 async def search_api(query):
     query = str(query)
-
-    async with AsyncSession() as session:
-        response = await session.get(
-            f"{API_URL}/search?q={query.replace(' ', '+')}&offset=0&type=track",
-            impersonate="chrome"
-        )
-        data = response.json()
-        print(data)
-        if "tracks" in data and len(data["tracks"]) > 0:
-            first_result = data["tracks"][0]
-            track_id = first_result["id"]
-            dl_response = await session.get(
-                f"{API_URL}/stream?trackId={track_id}",
-                impersonate="chrome"
-            )
-            dl_data = dl_response.json()
-            print(dl_data)
-            if "url" in dl_data:
-                title = first_result["title"]
-                duration = first_result.get("duration", "N/A")
-                link = dl_data["url"]
-                return title, duration, link
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'dnt': '1',
+        'priority': 'u=1, i',
+        'referer': 'https://dab.yeet.su/',
+        'sec-ch-ua': '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
+    }
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(
+            f"{API_URL}/search?q={query.replace(' ', '+')}&offset=0&type=track"
+        ) as response:
+            data = await response.json()
+            print(data)
+            if "tracks" in data and len(data["tracks"]) > 0:
+                first_result = data["tracks"][0]
+                track_id = first_result["id"]
+                async with session.get(
+                    f"{API_URL}/stream?trackId={track_id}"
+                ) as dl_response:
+                    dl_data = await dl_response.json()
+                    print(dl_data)
+                    if "url" in dl_data:
+                        title = first_result["title"]
+                        duration = first_result.get("duration", "N/A")
+                        link = dl_data["url"]
+                        return title, duration, link
 
     return None, None, None
 

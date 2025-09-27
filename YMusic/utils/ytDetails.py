9281c4fbd1fa5eb4784883545031dbade87e2a0c
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2025-09-27 15:14:11 +0530

    Update ytDetails.py

--- YMusic/utils/ytDetails.py
+++ YMusic/utils/ytDetails.py
@@ -26,7 +26,21 @@ def searchYt(query, is_videoId=False):
 
 async def search_api(query):
     query = str(query)
-    async with aiohttp.ClientSession() as session:
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'dnt': '1',
+        'priority': 'u=1, i',
+        'referer': 'https://dab.yeet.su/',
+        'sec-ch-ua': '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
+    }
+    async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(
             f"{API_URL}/search?q={query.replace(' ', '+')}&offset=0&type=track"
         ) as response:

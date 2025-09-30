6782478aa77600e69b4288c284108a685cf1b87d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-09-30 19:48:19 +0700

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -1,12 +1,12 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from xteam.configs import Var
+import config
 from ..logging import LOGGER
 
-api_id: int = Var.API_ID
-api_hash: str = Var.API_HASH
-session_string: str = Var.SESSION_STRING
+api_id: int = confg.API_ID
+api_hash: str = config.API_HASH
+session_string: str = config.SESSION_STRING
 
 YMusicBot = Client(
     name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string
